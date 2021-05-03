3a6e15536237c91b68a1cc60d834835c5ac21612
Elias <orekiyuta@gmail.com>
2021-05-03 23:58:39 +0800

    add something about emulator

---
+++ Emulator/windows10蓝屏.bat
@@ -0,0 +1,3 @@
+DISM /Online /Disable-Feature /FeatureName:"Containers-DisposableClientVM" 
+DISM /Online /Disable-Feature /FeatureName:"Microsoft-Hyper-V-All" 
+DISM /Online /Disable-Feature /FeatureName:"VirtualMachinePlatform"
